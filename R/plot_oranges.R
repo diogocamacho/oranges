fda89a7bad01406f3eb65702cce16d6c8053c7bf
Diogo Camacho <diogocamacho@mordor.private.wireless.med.harvard.edu>
2019-06-03 14:36:30 -0400

    re-wrote plotting function

--- R/plot_oranges.R
+++ R/plot_oranges.R
@@ -2,23 +2,35 @@
 #'
 #' \code{plot_oranges} returns a plot for the top 25 enriched pathways identified with the \code{\link{oranges}} package.
 #'
-#' @param oranges_res ORANGES data frame
+#' @param res ORANGES results data frame
 #' @param thr Significance (FDR-corrected q-value) for pathway enrichment.  Defaults to q < 0.05
-plot_oranges <- function(oranges_res,thr)
-{
-  if(missing(thr)) thr <- 0.05
+plot_oranges <- function(res, num_pathways) {
   
-  oranges_res %>% 
-    dplyr::filter(.,q_val < thr) %>% 
-    dplyr::slice(1:25) %>% 
-    ggplot(aes(y = -log10(q_val), x = fct_reorder(pathway_name, -log10(q_val)))) + 
-    geom_col(alpha = 0.8) + 
-    labs(y = "-log10(q value)", x = "Pathway") + 
-    coord_flip() + 
-    theme_bw() + 
-    theme(axis.title = element_text(face = "bold", colour = "black", size = 20),
-          axis.text = element_text(colour = "black", size = 12),
-          panel.grid = element_blank()) 
+  if(missing(num_pathways)) num_pathways <- 100
   
+  if(length(unique(res$data_source)) > 1) {
+    res %>% 
+      dplyr::filter(., padj != 1) %>%
+      dplyr::arrange(., padj) %>%
+      dplyr::slice(1:num_pathways) %>%
+      ggplot() + 
+      geom_point(aes(x = name, y = -log10(padj), color = gene_overlap, size = number_genes), alpha = 0.5) +
+      scale_color_viridis_c() + 
+      facet_grid(. ~ data_source, scales = "free") + 
+      theme_bw() + 
+      theme(axis.text.x = element_blank(),
+            panel.grid = element_blank())
+  } else {
+    res %>% 
+      dplyr::filter(., padj != 1) %>%
+      dplyr::arrange(., padj) %>%
+      dplyr::slice(1:num_pathways) %>%
+      ggplot() + 
+      geom_point(aes(x = name, y = -log10(padj), color = gene_overlap, size = number_genes), alpha = 0.5) +
+      scale_color_viridis_c() + 
+      theme_bw() + 
+      theme(axis.text.x = element_blank(),
+            panel.grid = element_blank())
+  }
 }
 
