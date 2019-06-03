9e61bef9ba8039107ea7e062f4d5f01e90839073
Diogo Camacho <diogocamacho@mordor.private.wireless.med.harvard.edu>
2019-06-03 14:36:14 -0400

    re-wrote oranges wrapper to be simpler and clearer

--- R/oranges.R
+++ R/oranges.R
@@ -7,42 +7,23 @@
 #' @param universe_entrez Full EntrezID set where the query set comes from (usually the full set of RNA-seq or microarray genes characterized in the transcriptomics platform)
 #' @param universe_symbols Gene symbols for the entire set of genes in universe
 #' @return A tibble.
-oranges <- function(query_entrez,universe_entrez,universe_names)
-{
-  # load pathway sets
-  load("./data/pathway_sets.RData")
+oranges <- function(query_entrez, universe_entrez, universe_names) {
 
-  # pathway matrix
-  P <- pathway_sets[[1]]
+  wp <- subset_pathways(query_entrez = query_entrez, universe_entrez = universe_entrez) #<-- returns ids (columns)
   
-  # pathway metadata
-  pathway_info <- pathway_sets[[2]]
+  eps <- extract_pathways(wp)
   
-  # clean up matrix & universe genes
-  keep_cols <- which(colnames(P) %in% universe_entrez)
-  keep_ids <- which(universe_entrez %in% colnames(P)[keep_cols])
-  universe_entrez <- universe_entrez[keep_ids]
-  universe_symbols <- universe_symbols[keep_ids]
-  P <- P[,keep_cols]
+  gid <- genes_pathway(genes_query = wp$genes_query, pathways_query = wp$pathways_query)
   
-  # clean matrix
-  nix <- Matrix::rowSums(P)
-  nix <- which(nix < 3 & nix > 500)
-  if (length(nix) != 0) {
-    P <- P[-nix, ]
-    pathway_info <- pathway_info[-nix, ]
-  }
+  enr <- calculate_enrichment(genes_query = wp$genes_query, 
+                              pathways_universe = wp$pathways_universe, 
+                              pathways_query = wp$pathways_query)
   
-  # perform enrichment
-  enr <- ora(query_set = query_entrez,
-             universe_entrez = universe_entrez,
-             universe_symbols = universe_symbols,pathway_matrix = P)
+  res <- enrichment_results(genes_pathway = gid, 
+                            number_genes = enr$number_genes, 
+                            gene_ratio = enr$gene_ratio, 
+                            nominal_p = enr$nominal_p, 
+                            p_adj = enr$padj)
   
-  enr <- enr %>% 
-    add_column(.,pathway_source = pathway_info$pathway_source,.before = 1) %>% 
-    add_column(.,pathway_name = pathway_info$pathway_name,.before = 2) %>%
-    add_column(.,q_val = p.adjust(enr$p_val,"fdr")) %>% 
-    dplyr::filter(.,gene_overlap != 0)
-
-  return(enr)
+  return(res)
 }
